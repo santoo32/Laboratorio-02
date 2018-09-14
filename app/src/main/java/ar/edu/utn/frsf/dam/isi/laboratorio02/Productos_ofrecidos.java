2116f69afd95ff637a716587030c5bc60cf50c58
Santiago Roa <santiagot.roa@gmail.com>
2018-09-13 21:12:05 -0300

    Creacion segunda activity

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/Productos_ofrecidos.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Productos_ofrecidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos_ofrecidos);
+    }
+}
