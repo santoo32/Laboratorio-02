220a7ed9658524be885dd6b9ff39841355ba06d5
Santiago Roa <santiagot.roa@gmail.com>
2018-09-25 22:30:10 -0300

    Punto 2 falta f) y g)

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/Productos_ofrecidos.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/Productos_ofrecidos.java
@@ -2,46 +2,69 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
 
 public class Productos_ofrecidos extends AppCompatActivity {
     private Spinner spinnerCat;
     private ListView listprod;
+    private Button agregar;
+    private EditText cantidad_pedir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos_ofrecidos);
         spinnerCat = (Spinner) findViewById(R.id.spinner_cat);
+        agregar = (Button) findViewById(R.id.btnProdAddPedido);
+        spinnerCat.setSelection(0);
         listprod = (ListView) findViewById(R.id.listprod);
+        cantidad_pedir = (EditText) findViewById(R.id.cantidad_pedir);
+        final ProductoRepository prodrepos = new ProductoRepository();
 
 
-
-        //adapter lista de categorias
-        ProductoRepository prodrepos = new ProductoRepository();
-        //aparentemente el
-        List<String> nombrescat = new ArrayList<>();
+        //---------Adapter lista de categorias-------------------------------------------------------------------
         List<Categoria> categorias = new ArrayList<>();
         categorias = prodrepos.getCategorias();
-        //como get categorias me devuelve una lista de categorias les saco los nombres como string
-        for(int i=0 ; i<prodrepos.getCategorias().size(); i++){
-            nombrescat.add(categorias.get(i).getNombre());
-        }
-        //inicializo el adapter y se lo paso a el spinner
-        ArrayAdapter<String> adaptador_categoria_prod = new ArrayAdapter<>(this , android.R.layout.simple_spinner_item, nombrescat);
+        ArrayAdapter<Categoria> adaptador_categoria_prod = new ArrayAdapter<>(this , android.R.layout.simple_spinner_item, categorias);
         adaptador_categoria_prod.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCat.setAdapter(adaptador_categoria_prod);
+        //--------Adapter producto por categoria---------------------------------------------------
+        spinnerCat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Categoria cat = prodrepos.getCategorias().get(position);
+                ArrayAdapter<Producto> adaptador_prod = new ArrayAdapter<Producto>(getApplicationContext(), android.R.layout.simple_spinner_item, prodrepos.buscarPorCategoria(cat));
+                adaptador_prod.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                listprod.setAdapter(adaptador_prod);
+                listprod.setItemChecked(0, true );
+            }
 
-        //adapter productos por categoria
-        //ArrayAdapter<String> adaptador_prodporcat = new ArrayAdapter<>(this , android.R.layout.simple_spinner_item, nombrescat);
-        //listprod.setAdapter(adaptador_prodporcat);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //i.putExtra("cantidad", cantidad_pedir.getText());
+                //i.putExtra("idProducto",idProducto);
+            }
+        });
+
     }
 }
