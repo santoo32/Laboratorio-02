4c16d474144ce7aee5bf0448f7511be8d8708b31
Santiago Roa <santiagot.roa@gmail.com>
2018-10-11 21:17:18 -0300

    Avances en punto 5 falta terminar el 3

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CustomAdapters/AdaptadorFilaHistorial.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CustomAdapters/AdaptadorFilaHistorial.java
@@ -5,94 +5,72 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import ar.edu.utn.frsf.dam.isi.laboratorio02.PedidoHolder;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.R;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
 
-public class AdaptadorFilaHistorial extends BaseAdapter implements ListAdapter {
-    private Context context;
-
-    //to store the products
-    private final Producto[] imageIDarray;
-
-
-
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-    // ESTO ES PARA PROBAR ALGO, NO DARLE BOLA
-
-
-
-    public AdaptadorFilaHistorial(ArrayList<String> list, Context context) {
-        this.list = list;
-        this.context = context;
-        imageIDarray = new Producto[0];
-
+public class AdaptadorFilaHistorial extends ArrayAdapter<Pedido> implements View.OnClickListener{
+    private Context ctx;
+    private List<Pedido> datos;
+    public AdaptadorFilaHistorial(Context context,List<Pedido> objects) {
+        super(context, 0, objects);
+        this.ctx = context;
+        this.datos = objects;
     }
 
-    @Override
-    public int getCount() {
-        return 0;
-    }
 
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
 
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-        if (view == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.activity_adaptadorfilahistorial, null);
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Pedido dataModel = getItem(position);
+
+        PedidoHolder viewHolder;
+
+        final View result;
+
+        if (convertView == null) {
+
+            viewHolder = new PedidoHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.row_item, parent, false);
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.name);
+            viewHolder.txtType = (TextView) convertView.findViewById(R.id.type);
+            viewHolder.txtVersion = (TextView) convertView.findViewById(R.id.version_number);
+            viewHolder.info = (ImageView) convertView.findViewById(R.id.item_info);
+
+            result=convertView;
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (PedidoHolder) convertView.getTag();
+            result=convertView;
         }
 
+        Animation animation = AnimationUtils.loadAnimation(mContext, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        result.startAnimation(animation);
+        lastPosition = position;
 
-
-        //Handle buttons and add onClickListeners
-        Button menuBtn = view.findViewById(R.id.menu_btn);
-        Button deleteBtn = view.findViewById(R.id.delete_btn);
-
-        deleteBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                //Chequear en que estado esta el pedido para ver si se puede cancelar
-
-
-                list.remove(position); //or some other task
-                notifyDataSetChanged();
-            }
-        });
-
-        menuBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                //mostrar detalles pedido
-
-                notifyDataSetChanged();
-            }
-        });
-        return view;
+        viewHolder.txtName.setText(dataModel.getName());
+        viewHolder.txtType.setText(dataModel.getType());
+        viewHolder.txtVersion.setText(dataModel.getVersion_number());
+        viewHolder.info.setOnClickListener(this);
+        viewHolder.info.setTag(position);
+        // Return the completed view to render on screen
+        return convertView;
     }
 }
