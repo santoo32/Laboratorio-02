6831f3f467fb604ebeef53637077fba540b8bd25
Unknown <ayr_12@hotmail.com>
2018-10-20 00:07:27 -0300

    Notificación creada
    Punto 2  de la parte 2 del lab 3 funcionando

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CustomAdapters/EstadoPedidoReceiver.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CustomAdapters/EstadoPedidoReceiver.java
@@ -1,12 +1,19 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02.CustomAdapters;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.Alta_pedidos;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.Historial_pedidos;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.R;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
 
@@ -17,16 +24,31 @@ public class EstadoPedidoReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
+
         Bundle extras = intent.getExtras();
         int id = extras.getInt("id_pedido");
 
         if (intent.getAction().equals(ESTADO_ACEPTADO)) {
             p1 = new PedidoRepository();
             Pedido ped = p1.buscarPorId(id);
-            Toast.makeText(context,"Pedido para " + ped.getMailContacto() + "ha cambiado de estado a aceptado",Toast.LENGTH_LONG).show();
+            //Toast.makeText(context,"Pedido para " + ped.getMailContacto() + "ha cambiado de estado a aceptado",Toast.LENGTH_LONG).show();
 
+            Intent i = new Intent(context,Historial_pedidos.class);
+            PendingIntent penInt = PendingIntent.getActivity(context,0,i,0);
+            //Creo la notificación
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "CANAL01")
+                    .setSmallIcon(R.drawable.logo)
+                    .setContentTitle("Tu pedido fue aceptado")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setStyle(new NotificationCompat.InboxStyle()
+                            .addLine("El costo sera "+ ped.total())
+                            .addLine("Previsto el envio para "+ped.getFecha()))
+                    .setContentIntent(penInt)
+                    .setAutoCancel(true);
+
+            //Muestro la notificación
+            NotificationManagerCompat notManager = NotificationManagerCompat.from(context);
+            notManager.notify(99,mBuilder.build());
         }
     }
 }
--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
@@ -1,6 +1,9 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -37,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Llamo al método para crear el canal de notificaciones
+        createNotificationChannel();
 
         btnNuevoPedido = (Button) findViewById(R.id.btnMainNuevoPedido);
 
@@ -70,6 +75,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+    //Creación del canal - punto 2 lab 3
+    private void createNotificationChannel() {
+        // Crear el canal de notificaciones pero solo para API 26 io superior
+        // dado que NotificationChannel es una clase nueva que no está incluida
+        // en las librerías de soporte que brindan compatibilidad hacía atrás
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.canal_estado_nombre);
+            String description = getString(R.string.canal_estado_descr);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CANAL01", name, importance);
+            channel.setDescription(description);
+            // Registrar el canal en el sistema
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
